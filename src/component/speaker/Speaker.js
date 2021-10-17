e30fff9d7427eb3976095f0bad86ed9b2cb43e6a
Abdus Sabur <saburali7264@gmail.com>
2021-10-17 20:41:09 +0600

    Create and Use Fake Data

--- src/component/speaker/Speaker.js
+++ src/component/speaker/Speaker.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './speaker.css';
 import {AiOutlineShoppingCart} from "react-icons/all";
 
 const Speaker = () => {
+
+    //   JSON API CALL
+    const [speaker, setSpeaker] = useState([]);
+    useEffect(() => {
+        fetch("./fakeUserData.json")
+            .then((res) => res.json())
+            .then((data) => setSpeaker(data));
+    }, []);
+
     return (
         <div className="speakers">
             <div className="card mb-4">
@@ -43,129 +52,6 @@ const Speaker = () => {
                     <button type="button" className="btn" title="Add to Cart">
                         <AiOutlineShoppingCart className="icon" />
                         <span> Add to cart </span>
-                    </button>
-                </div>
-            </div>
-            <div className="card mb-4">
-                <div className="img-container">
-                    <img src="https://pro-img.saburali.me/programming-hero/tech-conference/02.jpg" className=""
-                         alt=""/>
-                </div>
-                <div className="card-body">
-                    <p className="mb-1 position" title="">
-                        Position:
-                        <span className="themeLetter"> Founder </span>
-                    </p>
-                    <h5 className="card-title themeLetter" title=""> Troy Jimene </h5>
-                    <div className="contact">
-
-                        <div className="props">
-                            <h5 className="themeLetter card-contact"> Birthday -  </h5>
-                            <h5 className="themeLetter card-contact"> Phone -  </h5>
-                            <h5 className="themeLetter card-contact"> Address -  </h5>
-                            <h5 className="themeLetter card-contact"> Email -  </h5>
-                        </div>
-                        <div>
-                            <h5 className="card-contact themeLetter" title=""> 12/5/1982 </h5>
-                            <h5 className="card-contact themeLetter" title=""> (500)-242-4394 </h5>
-                            <h5 className="card-contact themeLetter" title=""> 6172 Mcgowen St </h5>
-                            <h5 className="card-contact themeLetter" title=""> troy.jimenez@saburbd.com </h5>
-                        </div>
-                    </div>
-                    <div className="media">
-                        <h2 className="align-self-center mr-3 price" title=""> $65446 </h2>
-                        <div className="media-body">
-                            <div style={{width: '90px', textAlign: 'left'}}>
-                                <p className="themeLetter" title="Sex"> Male </p>
-                                <p className="themeLetter" style={{marginBottom: '0'}} title="Age"> Age: 22 </p>
-                            </div>
-                        </div>
-                    </div>
-                    <button type="button" className="btn" title="Add to Cart">
-                        <AiOutlineShoppingCart className="icon" />
-                        Add to cart
-                    </button>
-                </div>
-            </div>
-            <div className="card mb-4">
-                <div className="img-container">
-                    <img src="https://pro-img.saburali.me/programming-hero/tech-conference/04.jpg" className=""
-                         alt=""/>
-                </div>
-                <div className="card-body">
-                    <p className="mb-1 position" title="">
-                        Position:
-                        <span className="themeLetter"> Founder </span>
-                    </p>
-                    <h5 className="card-title themeLetter" title=""> Troy Jimene </h5>
-                    <div className="contact">
-
-                        <div className="props">
-                            <h5 className="themeLetter card-contact"> Birthday -  </h5>
-                            <h5 className="themeLetter card-contact"> Phone -  </h5>
-                            <h5 className="themeLetter card-contact"> Address -  </h5>
-                            <h5 className="themeLetter card-contact"> Email -  </h5>
-                        </div>
-                        <div>
-                            <h5 className="card-contact themeLetter" title=""> 12/5/1982 </h5>
-                            <h5 className="card-contact themeLetter" title=""> (500)-242-4394 </h5>
-                            <h5 className="card-contact themeLetter" title=""> 6172 Mcgowen St </h5>
-                            <h5 className="card-contact themeLetter" title=""> troy.jimenez@saburbd.com </h5>
-                        </div>
-                    </div>
-                    <div className="media">
-                        <h2 className="align-self-center mr-3 price" title=""> $65446 </h2>
-                        <div className="media-body">
-                            <div style={{width: '90px', textAlign: 'left'}}>
-                                <p className="themeLetter" title="Sex"> Male </p>
-                                <p className="themeLetter" style={{marginBottom: '0'}} title="Age"> Age: 22 </p>
-                            </div>
-                        </div>
-                    </div>
-                    <button type="button" className="btn" title="Add to Cart">
-                        <AiOutlineShoppingCart className="icon" />
-                        Add to cart
-                    </button>
-                </div>
-            </div>
-            <div className="card mb-4">
-                <div className="img-container">
-                    <img src="https://pro-img.saburali.me/programming-hero/tech-conference/05.jpg" className=""
-                         alt=""/>
-                </div>
-                <div className="card-body">
-                    <p className="mb-1 position" title="">
-                        Position:
-                        <span className="themeLetter"> Founder </span>
-                    </p>
-                    <h5 className="card-title themeLetter" title=""> Troy Jimene </h5>
-                    <div className="contact">
-
-                        <div className="props">
-                            <h5 className="themeLetter card-contact"> Birthday -  </h5>
-                            <h5 className="themeLetter card-contact"> Phone -  </h5>
-                            <h5 className="themeLetter card-contact"> Address -  </h5>
-                            <h5 className="themeLetter card-contact"> Email -  </h5>
-                        </div>
-                        <div>
-                            <h5 className="card-contact themeLetter" title=""> 12/5/1982 </h5>
-                            <h5 className="card-contact themeLetter" title=""> (500)-242-4394 </h5>
-                            <h5 className="card-contact themeLetter" title=""> 6172 Mcgowen St </h5>
-                            <h5 className="card-contact themeLetter" title=""> troy.jimenez@saburbd.com </h5>
-                        </div>
-                    </div>
-                    <div className="media">
-                        <h2 className="align-self-center mr-3 price" title=""> $65446 </h2>
-                        <div className="media-body">
-                            <div style={{width: '90px', textAlign: 'left'}}>
-                                <p className="themeLetter" title="Sex"> Male </p>
-                                <p className="themeLetter" style={{marginBottom: '0'}} title="Age"> Age: 22 </p>
-                            </div>
-                        </div>
-                    </div>
-                    <button type="button" className="btn" title="Add to Cart">
-                        <AiOutlineShoppingCart className="icon" />
-                        Add to cart
                     </button>
                 </div>
             </div>
