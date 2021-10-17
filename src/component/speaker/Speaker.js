51d2daa23d939b8c2e1f800ba1535f01ba17c2d6
Abdus Sabur <saburali7264@gmail.com>
2021-10-17 13:14:14 +0600

    Create 'Speakers & Speaker' Component

---
+++ src/component/speaker/Speaker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './speaker.css';
+
+const Speaker = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Speaker;
---
+++ src/component/speakers/Speakers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './speakers.css';
+
+const Speakers = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Speakers;
