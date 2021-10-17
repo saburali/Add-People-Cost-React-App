8e4a59a2ea6d128e01318611500b3001c9e31853
Abdus Sabur <saburali7264@gmail.com>
2021-10-17 22:22:53 +0600

    Cart Section Dynamic

--- src/component/cart/Cart.js
+++ src/component/cart/Cart.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import './cart.css';
+import {FaDollarSign, FaUser} from "react-icons/all";
 
-const Cart = () => {
+const Cart = (props) => {
+
+    // Destructuring Cart 
+    const { cart } = props;
+
+    // Using Reduce For Cart Calculation
+    const totalReducer = (preValue, currentValue) => preValue + currentValue.Salary;
+    const finalTotal = cart.reduce(totalReducer, 0);
+    const total = parseFloat(finalTotal);
+
+    // FontAwesome icon Declare
+    const user = <FaUser style={{fontSize: '16px'}}/>;
+    const dollarSign = <FaDollarSign style={{fontSize: '18px', marginBottom: '-2px'}}/>;
+    
     return (
         <div className="cart">
             <div className="card">
                 <h2 className="themeLetter" style={{marginTop: '0'}}> Speaker Cart </h2>
                 <div className="cart-container">
                     <div className="property">
-                        <h4 className="themeLetter"> Speaker : </h4>
-                        <p className="themeLetter"> Total Cost : </p>
+                        <h4 className="themeLetter"> {user} Speaker : </h4>
+                        <p className="themeLetter"> {dollarSign} Total Cost : </p>
                     </div>
                     <div className="value">
-                        <h4> 2 </h4>
-                        <p> $868.88 </p>
+                        <h4> {cart.length} </h4>
+                        <p> {dollarSign}{total} </p>
                     </div>
                 </div>
                 <div className="person">
-                    <p> 1. Jiyaur Rahman </p>
-                    <p> 2. Jiyaur Rahman </p>
-                    <p> 3. Jiyaur Rahman </p>
+                    <p> {user} Speaker Name - </p>
+
+                    <ol type="1">
+                        {cart.map((speaker) => (
+                            <li>{speaker.name}</li>
+                        ))}
+                    </ol>
+
+                    {/*<p> 1. Jiyaur Rahman </p>*/}
+                    {/*<p> 2. Jiyaur Rahman </p>*/}
+                    {/*<p> 3. Jiyaur Rahman </p>*/}
                 </div>
             </div>
         </div>
