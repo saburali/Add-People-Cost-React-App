c5450fbf630d998cb4ac43cde9d2fc9e2a9bbedf
Abdus Sabur <saburali7264@gmail.com>
2021-10-17 23:32:19 +0600

    Somting Design Cng

--- src/component/header/DownHeader.js
+++ src/component/header/DownHeader.js
@@ -6,18 +6,14 @@ const DownHeader = () => {
     return (
         <div className="down-header">
             <div className="imgBG" style={{backgroundImage: "url(" + bgImg + ")"}}></div>
-            <div className="app-container" style={{display: 'flex'}}>
+            <div className="app-container">
                 <div className="down-content">
                     <h2 className="title"><span style={{fontWeight: '300'}}> Welcome to </span> Tech in
                         Asia <br/> Conference 2021 </h2>
                     <div className="content">
-                        <div style={{textAlign: 'left'}}>
+                        <div style={{textAlign: 'center'}}>
                             <h3> Project Budget $259M </h3>
-                            <p> 12-14 OCTOBER 2021 </p>
-                            <p> Learn how to harness Southeast Asia's booming tech and startup ecosystem at Tech in Asia
-                                Conference 2021. </p>
-                            <p style={{marginBottom: '0'}}> This application was created for project purposes only.
-                                Please don't take it seriously. </p>
+                            <p style={{paddingBottom: '0'}}> 12-14 OCTOBER 2021 </p>
                         </div>
                     </div>
                 </div>
